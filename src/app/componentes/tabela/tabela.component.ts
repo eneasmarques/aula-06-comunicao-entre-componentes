c6b4b9f9ed9e86e1d7a8b9d1cbb31f8b37a85dbc
Enéas Marques <eneasmarques@outlook.com.br>
2023-12-20 09:32:44 -0300

    feat(Tabela): cria lista de notas

--- src/app/componentes/tabela/tabela.component.ts
+++ src/app/componentes/tabela/tabela.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Aluno } from '../../modelo/Aluno';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tabela',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tabela.component.html',
-  styleUrl: './tabela.component.css'
+  styleUrl: './tabela.component.css',
 })
 export class TabelaComponent {
-
+  @Input() alunos: Aluno[] = [];
 }
